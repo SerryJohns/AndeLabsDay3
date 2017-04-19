382f64f29856f20a19ce22f16b4c1bee0708cd5a
SerryJohns <serembaj@gmail.com>
2017-04-19 15:53:04 +0300

    Print word occurances

---
+++ wordcount.py
@@ -0,0 +1,18 @@
+def words(param):
+    result = {}
+    # Deal with multiple spaces
+    proper_sentence = ' '.join(param.split())
+
+    # Create a list containing word items
+    proper_words = proper_sentence.split(" ")
+
+    for my_word in proper_words:
+        if my_word in result:
+            continue
+        try:
+            result[int(my_word)] = proper_words.count(my_word)
+        except:
+            result[my_word] = proper_words.count(my_word)
+    return result
+
+print(words('car : carpet as java : javascript!!&@$%^&'))
